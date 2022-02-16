deee7f5d5e0e829aece477bb35b2c94000c6be3f
Razib-Debnath16 <razib.debnath016@gmail.com>
2022-02-17 02:35:34 +0600

    calculate total expenses and remaining balance

--- JS2/money.js
+++ JS2/money.js
@@ -2,11 +2,20 @@ function getBalance(product) {
     var incomeInput = document.getElementById('income' + product);
     const income = parseInt(incomeInput.value);
     var foodInput = document.getElementById('food' + product);
-    const foodCost = parseInt(foodInput.innerText);
+    const foodCost = parseInt(foodInput.value);
     var rentInput = document.getElementById('rent' + product);
-    const rentCost = parseInt(rentInput.innerText);
+    const rentCost = parseInt(rentInput.value);
     var clothInput = document.getElementById('cloth' + product);
-    const clothCost = parseInt(clothInput.innerText);
+    const clothCost = parseInt(clothInput.value);
+    var totalExpenses = foodCost + rentCost + clothCost;
+    if (totalExpenses > income) {
+        console.log(alert('Your Expenses are more than your income'));
+    }
+    else {
+        const balance = income - totalExpenses;
+        document.getElementById('total-expenses').innerText = totalExpenses;
+        document.getElementById('remaining-balance').innerText = balance;
+    }
 
 }
 document.getElementById('calculate-btn').addEventListener('click', function () {
