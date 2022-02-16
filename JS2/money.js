7aa08511f05b1ac4051732368da8ab33a11cf00c
Razib-Debnath16 <razib.debnath016@gmail.com>
2022-02-17 03:13:07 +0600

    calculate saving amount and remaining balance

--- JS2/money.js
+++ JS2/money.js
@@ -1,3 +1,5 @@
+// Get the balance after expenses
+
 function getBalance(product) {
     var incomeInput = document.getElementById('income' + product);
     const income = parseInt(incomeInput.value);
@@ -9,15 +11,31 @@ function getBalance(product) {
     const clothCost = parseInt(clothInput.value);
     var totalExpenses = foodCost + rentCost + clothCost;
     if (totalExpenses > income) {
-        console.log(alert('Your Expenses are more than your income'));
+        console.log(alert('Your Expenses are more than your income.Please,ensure you are enter correct number'));
     }
     else {
         const balance = income - totalExpenses;
         document.getElementById('total-expenses').innerText = totalExpenses;
         document.getElementById('remaining-balance').innerText = balance;
+        return balance;
     }
 
 }
+
+// calculate saving amount and remaining balance
+
+function getRemainingBalance() {
+    var savingInput = document.getElementById('save-input');
+    const savingPercentage = parseFloat(savingInput.value);
+    var savingAmount = (parseInt(document.getElementById('income-input').value) * savingPercentage) / 100;
+    document.getElementById('saving-amount').innerText = savingAmount;
+    document.getElementById('remaing-balance').innerText = getBalance('-input') - savingAmount;
+}
+// using addEventListener on calculate button
 document.getElementById('calculate-btn').addEventListener('click', function () {
-    var RemainingBalance = getBalance('-input');
+    var afterExpenseBalance = getBalance('-input');
 });
+// using addEventListener on save button
+document.getElementById('save-btn').addEventListener('click', function () {
+    var remainingBalance = getRemainingBalance();
+})
